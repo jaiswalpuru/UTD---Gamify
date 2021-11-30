cc04c069a53b19394ee25e5aaa2b80d84a8e770a
Puru Jaiswal <purujaiswal1996@gmail.com>
2021-11-30 16:08:14 -0600

    room booking logic

--- utd-gamify/server/routes/feature.js
+++ utd-gamify/server/routes/feature.js
@@ -1,13 +1,12 @@
 var createError = require('http-errors');
 var express = require("express");
 var router = express.Router();
-var helpers = require('../helpers/helpers.js');
 
 var monk = require('monk');
 var db = monk('localhost:27017/utd-gamify');
 var locationColl = db.get('location');
 var roomsColl = db.get('room');
-
+var bookings = db.get('bookings');
 
 function isEmptyObject(obj) {
     if (Object.keys(obj).length === 0) {
@@ -16,7 +15,22 @@ function isEmptyObject(obj) {
     return false;
   }
   
-
+function insertInMongo(obj) {
+    try {
+        bookings.insert({
+            netId:obj.netId,
+            roomNum:obj.roomNum,
+            timeStart:obj.timeStart,
+            timeDuration:obj.timeDuration,
+        },(err, res)=>{
+            if (err) throw err;
+            return res;
+        });
+    } catch (err) {
+        console.log(err);
+    }
+    return null;
+}
 
 // return the all the locations
 router.get('/locations', (req, res) => {
@@ -35,17 +49,35 @@ router.post('/book', (req, res, next) => {
 
     if (isEmptyObject(req.body)) {
         res.sendStatus(400)
-    }else{
-        console.log(req.body);
+    } else { 
         try {
             let bookedBy = req.body.netId.trim();
             let roomNumber = req.body.roomNum;
             let timeDuration = req.body.timeDuration;
             let timeStart = Date.now();
+
+            let obj = {
+                netId : bookedBy,
+                roomNum : roomNumber,
+                timeStart : timeStart,
+                timeDuration : timeDuration,
+            }
+
             roomsColl.find({'roomNum':parseInt(roomNumber)},(err, result)=>{
                 if (err) throw err;
+
                 if (result.length === 0) {
-                    console.log("ere I a");
+                    insertInMongo(obj);
+                    res.json(obj);
+                } else {
+                    let prevTimeStart = result.timeStart;
+                    let prevTimeDuration = result.timeDuration;
+                    if (prevTimeStart+prevTimeDuration < timeStart) {
+                        res.json({"message":"Room is already booked"});
+                    } else {
+                        insertInMongo(obj);
+                        res.json(obj);
+                    }
                 }
             });
         } catch(err) {
@@ -61,7 +93,7 @@ router.get('/room/:id', (req, res) => {
             if (err) throw  err;
             res.json(result);
         });
-    } catch(err){
+    } catch(err) {
         console.log(err);
     }
 });
